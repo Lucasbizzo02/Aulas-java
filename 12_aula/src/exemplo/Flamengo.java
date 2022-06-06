f0ea25e70577439dda286702351f393e09d2e76f
Lucasbizzo02 <101873007+Lucasbizzo02@users.noreply.github.com>
2022-06-06 11:15:31 -0300

    Add files via upload

---
+++ 12_aula/src/exemplo/Flamengo.java
@@ -0,0 +1,21 @@
+package exemplo;
+
+public class Flamengo implements exercicio {
+
+	@Override
+	public String nomeTime() {
+		return "Flamengo";
+	}
+
+	@Override
+	public String elenco() {
+		return "elenco";
+	}
+
+	@Override
+	public int valor() {
+		return 200;
+	}
+	
+
+}
---
+++ 12_aula/src/exemplo/exercicio.java
@@ -0,0 +1,8 @@
+package exemplo;
+
+public interface exercicio {
+	public String nomeTime();
+	public String elenco();
+	public int valor();
+
+}
