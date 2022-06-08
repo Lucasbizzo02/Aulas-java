125cf8e1ca8b960b16f55ef041ed488c497b18f7
Lucasbizzo02 <101873007+Lucasbizzo02@users.noreply.github.com>
2022-06-08 11:35:46 -0300

    Add files via upload

---
+++ 13_aula/src/main/java/app/App.java
@@ -0,0 +1,25 @@
+package app;
+
+
+import java.util.List;
+
+
+import dao.UsuarioDAO;
+import model.Usuario;
+
+public class App {
+
+	public static void main(String[] args) {
+		List<Usuario>lista;
+		
+		UsuarioDAO dao = new UsuarioDAO();
+		
+		lista = dao.listar();
+		
+		for(Usuario u: lista) {
+			System.out.println(u);
+		}
+
+	}
+
+}
